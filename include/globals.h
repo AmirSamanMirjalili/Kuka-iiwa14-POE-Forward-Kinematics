3f0419ebe3bf5095233e6ac56451bd815c9edf45
AmirSamanMirjalili <sammj2231@gmail.com>
2024-07-17 22:02:43 +0330

    added

---
+++ include/globals.h
@@ -0,0 +1,30 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <GLFW/glfw3.h>
+#include <mujoco/mujoco.h>
+
+const double CTRL_UPDATE_FREQ = 100.0;
+
+
+extern const char* FILENAME;
+extern bool debug_mode;
+extern int* actuator_ids;
+extern mjvCamera cam;
+extern mjvScene scn;
+extern mjrContext con;
+extern bool button_left, button_middle, button_right;
+extern double lastx, lasty;
+extern bool isPushing;
+extern int grabbedBodyId;
+extern mjtNum pushForce[3];
+extern double pushStartX, pushStartY;
+extern mjModel* m;
+extern mjData* d;
+extern mjvPerturb pert;
+extern int mocap_body_id;
+extern bool use_zero_control;
+extern bool kinematic_debug_mode;
+extern bool use_test_control;
+
+#endif // GLOBALS_H
